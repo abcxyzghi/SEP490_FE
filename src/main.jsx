5eea9c139576983b30f5431cfbf38434df246ca9
Thanh Nguyen Hoang <nguyenhoangthanh30703@gmail.com>
2025-06-01 16:02:49 +0700

    update react-router-dom to version 7.6.1 and add login/register pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import LoginPage from './page/login/index.jsx';
+import RegisterPage from './page/register/index.jsx';
+  const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage/>,
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
