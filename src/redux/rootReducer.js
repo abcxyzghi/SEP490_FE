b35ca91f091f492adfe80a4e14c3454bbb745e27
Thanh Nguyen Hoang <nguyenhoangthanh30703@gmail.com>
2025-06-02 10:56:48 +0700

    refactor rootReducer for cleaner formatting

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,14 +2,9 @@ import { combineReducers } from "@reduxjs/toolkit";
 import  userReducer  from "./features/userSlice";
 import  cartReducer  from "./features/cartSlice";
 
-
-
-const rootReducer = combineReducers({
-    
+const rootReducer = combineReducers({    
         user: userReducer,
         cart: cartReducer
-
-
 })
 
 export default rootReducer;
