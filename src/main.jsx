2c31427e04925ec3d0e9c5710b694c377b61863d
LeBMinh <155410101+LeBMinh@users.noreply.github.com>
2025-06-04 01:18:05 +0700

    basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import LoginPage from './page/login/index.jsx';
-import RegisterPage from './page/register/index.jsx';
-  const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage/>,
-  },
-]);
+
 createRoot(document.getElementById('root')).render(
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>,
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+  )
+
+
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+// import LoginPage from './page/login/index.jsx';
+// import RegisterPage from './page/register/index.jsx';
+//   const router = createBrowserRouter([
+//   {
+//     path: "/login",
+//     element: <LoginPage />,
+//   },
+//   {
+//     path: "/register",
+//     element: <RegisterPage/>,
+//   },
+// ]);
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>,
+// )
