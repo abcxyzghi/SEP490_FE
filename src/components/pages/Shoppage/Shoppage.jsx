6711bfa350a603ae0f058e0d1853e95bf1910594
LeBMinh <155410101+LeBMinh@users.noreply.github.com>
2025-06-18 18:03:05 +0700

    homepage

--- src/components/pages/Shoppage/Shoppage.jsx
+++ src/components/pages/Shoppage/Shoppage.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ImageCarousel from '../../libs/Carousel/Carousel';
 
 export default function Shoppage() {
   return (
-    <div style={{marginTop: '200px'}}>Shoppage</div>
+    <div>
+      <ImageCarousel/>
+    </div>
   )
 }
