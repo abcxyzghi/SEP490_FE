506a374937274c1c9fe71c97deffc6648ea6867a
Thanh Nguyen Hoang <nguyenhoangthanh30703@gmail.com>
2025-06-20 23:54:45 +0700

    Footer done

--- src/components/libs/Footer/Footer.jsx
+++ src/components/libs/Footer/Footer.jsx
@@ -1,34 +1,56 @@
 import React from 'react';
-import './Footer.css'
-// import media
-import FullLogoGrD from '../../../assets/logoSVG/Full_logo-Grdient.svg';
-import About from '../../../assets/others/about_ftr.svg';
-import Information from '../../../assets/others/information_ftr.svg';
-import Address from '../../../assets/Icon_fill/Pin_fill.svg';
-import Email from '../../../assets/Icon_fill/Send_fill.svg';
-import Phone from '../../../assets/Icon_fill/Phone_fill.svg';
-
-export default function Footer() {
-    return (
-        <div className='footer-container'>
-            <div className='footer-infos'>
-                <div className='footer-logo-container'>
-
-                </div>
-
-                <div className='footer-about-container'>
-
-                </div>
-
-                <div className='footer-information-container'>
-
-                </div>
-
-            </div>
-            
-            <div className='footer-rights'>
-
-            </div>
+import './Footer.css';
+import { FaMapMarkerAlt, FaEnvelope, FaPhone } from 'react-icons/fa';
+import logo from '../../../assets/logoSVG/Full_logo-Grdient.svg';
+import StreetIcon from '../../../assets/Icon_fill/Pin_fill.svg';
+import SendIcon from '../../../assets/Icon_fill/Send_fill.svg';
+import PhoneIcon from '../../../assets/Icon_fill/Phone_fill.svg';
+import AboutIcon from '../../../assets/icon/About_Icon.svg';
+import InformationIcon from '../../../assets/icon/Information_Icon.svg';
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src={logo} alt="Manga Mystery Box Logo" />
         </div>
-    )
-}
+
+        <div className="footer-about">
+          <div className="footer-title">
+            <img src={AboutIcon} alt="icon" className="icon" /> About
+          </div>
+          <div className="footer-info">
+            <p> <img src={StreetIcon} alt="icon" className="icon" /> 7 D. D1, Long Thạnh Mỹ, Thủ Đức, Hồ Chí Minh</p>
+            <p><img src={SendIcon} alt="icon" className="icon" /> MMB@gmail.com</p>
+            <p><img src={PhoneIcon} alt="icon" className="icon" />0988776655</p>
+          </div>
+        </div>
+
+        <div className="footer-info-links">
+          <div className="footer-title">
+            <img src={InformationIcon} alt="icon" className="icon" /> Information
+          </div>
+          <div className="footer-links">
+            <div>
+              <p>About</p>
+              <p>Product</p>
+              <p>Contact</p>
+              <p>Help Center</p>
+            </div>
+            <div>
+              <p>Terms Of Service</p>
+              <p>Privacy Policy</p>
+              <p>Copyright Policy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        ©2025 MangaMysteryBox | All Rights Reserved
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
