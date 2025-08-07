7104644a437d7dd78c95ed79df5f860f0b84ef64
Thanh Nguyen Hoang <nguyenhoangthanh30703@gmail.com>
2025-08-07 09:51:31 +0700

    fix the end point of fetchMyAuctionList

--- src/services/api.auction.js
+++ src/services/api.auction.js
@@ -19,13 +19,13 @@ export const fetchMyAuctionList = async () => {
   try {
     const response = await pythonApiWithFallback({
       method: "get",
-      url: "/api/auction/all",
+      url: "/api/auction/me",
       requiresAuth: true, 
     });
 
     return response.data;
   } catch (error) {
-    console.error("Fetch auction list failed:", error);
+    console.error("Fetch my auction list failed:", error);
     throw error;
   }
 };
